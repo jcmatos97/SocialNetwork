061661f94477e949c4301e75d911f6cb5475c50e
jcmatos97 <jcmatos97@outlook.com>
2018-08-31 08:58:11 -0400

    Limpiando proyecto

---
+++ PruebaAPI/Controllers/IdentityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var x = new { newe = "dd", message = "sss" };
+            //return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+            return new JsonResult(x);
+        }
+    }
+}
